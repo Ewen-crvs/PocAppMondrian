52525295ffb564df2a26d8b1965e8fb05531c9f2
Kryyto <69859497+Kryyto@users.noreply.github.com>
2024-10-10 16:53:57 +0200

    improve style of admin.vue & add create button

--- pages/admin.vue
+++ pages/admin.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-[#404040] bg-cover bg-center w-auto h-auto">
-    <div class="pb-20 bg-[url('/public/assets/image2.svg')] bg-cover bg-center w-auto h-auto">
-      <div class="bg-gray-700/50 bg-cover bg-center w-auto h-auto">
+    <div class="bg-[url('/public/assets/image2.svg')] bg-cover bg-center w-auto h-auto">
+      <div class="pb-20 bg-gray-700/50 bg-cover bg-center w-auto h-auto">
         <div class="max-w-xs mx-auto">
           <!-- Logo -->
           <div class="justify-center flex pt-10">
@@ -13,11 +13,14 @@
             <div class="flex justify-between">
               <p>Q.{{ questionIndex + 1 }}:</p>
               <div class="flex">
-                <button class="mr-2 rounded-md bg-slate-900/70 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Editer</button>
-                <button class="rounded-md bg-slate-900/70 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Supprimer</button>
+                <button class="mr-2 rounded-md bg-[#F1ECD4] px-3 py-1.5 text-sm font-semibold leading-6 text-black shadow-sm hover:bg-yellow-200">Editer</button>
+                <button class="rounded-md bg-[#F1ECD4] px-3 py-1.5 text-sm font-semibold leading-6 text-black shadow-sm hover:bg-red-500">Supprimer</button>
               </div>
             </div>
             <p>{{ question.title }}</p>
+          </div>
+          <div class="mt-12">
+            <button class="w-full justify-center rounded-md bg-[#F1ECD4] px-3 py-1.5 text-sm font-semibold leading-6 text-black shadow-sm hover:bg-yellow-200">Créer un contenu</button>
           </div>
         </div>
       </div>
--- pages/index.vue
+++ pages/index.vue
@@ -24,7 +24,7 @@
                 </div>
 
                 <div class="mt-12">
-                  <button @click.prevent="onSubmit()" class="flex w-full justify-center rounded-md bg-slate-900/70 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Commencer</button>
+                  <button @click.prevent="onSubmit()" class="flex w-full justify-center rounded-md bg-[#F1ECD4] px-3 py-1.5 text-sm font-semibold leading-6 text-black shadow-sm hover:bg-yellow-200">Commencer</button>
                 </div>
               </form>
             </div>
